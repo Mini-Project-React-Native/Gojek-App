49abdb018a6584d2748704c80b57b7a9a3b37a2b
Fawwaazrahman Arandhana <fawwaazrahman2002@gmail.com>
2020-09-27 05:01:30 +0700

    Fixed RNCSafeView Not Found In The UIManager

--- App.js
+++ App.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
-import { Button, View } from 'react-native';
-
 import Routers from './src/config/Routers';
 import { LoginPage } from './src/containers/pages';
-
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: false
-    }
+      isLoggedIn: false,
+    };
   }
 
   loggedIn = () => {
     this.setState({
-      isLoggedIn: !this.state.isLoggedIn
-    })
-  }
+      isLoggedIn: !this.state.isLoggedIn,
+    });
+  };
 
-  render() { 
+  render() {
     return (
       <>
-        {this.state.isLoggedIn ?
+        {this.state.isLoggedIn ? (
           <Routers />
-          :
+        ) : (
           <LoginPage login={this.loggedIn} />
-        }
+        )}
       </>
     );
   }
