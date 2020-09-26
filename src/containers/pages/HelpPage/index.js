7a11d14270a50b18b573951087e8d85825010463
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-26 22:39:53 +0700

    new page

--- src/containers/pages/HelpPage/index.js
+++ src/containers/pages/HelpPage/index.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
+
+class HelpPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <View>
+                <Text>HelpPage</Text>
+            </View>
+        );
+    }
+}
+ 
+export default HelpPage;
