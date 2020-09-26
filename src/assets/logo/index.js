be4c5d05d34ec05efef2290525ec23a9ad5a4643
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-26 22:45:37 +0700

    adding logo

--- src/assets/logo/index.js
+++ src/assets/logo/index.js
@@ -2,5 +2,7 @@ import GoFoodLogo from './go-food.png';
 import GojekLogo from './gojek.png';
 import GopayBlueWhiteLogo from './gopay-blue-white.png';
 import WhiteLogo from './white.png';
+import GojekTrpLogo from './gojektrp.png';
+import LogoGojekPutih from './logojek-putih.png';
 
-export { GoFoodLogo, GojekLogo, GopayBlueWhiteLogo, WhiteLogo };
+export { GoFoodLogo, GojekLogo, GopayBlueWhiteLogo, WhiteLogo, GojekTrpLogo, LogoGojekPutih };
