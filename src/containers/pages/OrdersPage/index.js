590d7713221941d0c3046b3160280ead8faa278e
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-26 22:35:18 +0700

    new page

--- src/containers/pages/OrdersPage/index.js
+++ src/containers/pages/OrdersPage/index.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
+
+class OrdersPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <View>
+                <Text>OrdersPage</Text>
+            </View>
+        );
+    }
+}
+ 
+export default OrdersPage;
