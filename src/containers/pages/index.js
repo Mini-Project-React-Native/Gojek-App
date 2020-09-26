8efb5e419aaf88e4016be42650f99577f7c2eb9d
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-26 15:51:12 +0700

    index for all pages

---
+++ src/containers/pages/index.js
@@ -0,0 +1,8 @@
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import AccountPage from './AccountPage';
+import HelpPage from './HelpPage';
+import InboxPage from './InboxPage';
+import OrdersPage from './OrdersPage';
+
+export { HomePage, LoginPage, AccountPage, HelpPage, InboxPage, OrdersPage };
