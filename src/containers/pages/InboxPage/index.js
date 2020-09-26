0902a81f7eba2c75d10750b9c3b03d4f8e318102
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-26 22:38:06 +0700

    new page

--- src/containers/pages/InboxPage/index.js
+++ src/containers/pages/InboxPage/index.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
+
+class InboxPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <View>
+                <Text>InboxPage</Text>
+            </View>
+        );
+    }
+}
+ 
+export default InboxPage;
