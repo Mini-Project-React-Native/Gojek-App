cd237fce52100ec848c0e0aaf2ee322025aa2697
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-26 22:40:38 +0700

    new page

--- src/containers/pages/AccountPage/index.js
+++ src/containers/pages/AccountPage/index.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
+
+class AccountPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <View>
+                <Text>AccountPage</Text>
+            </View>
+        );
+    }
+}
+ 
+export default AccountPage;
