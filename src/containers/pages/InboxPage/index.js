e44dd5bb743d885e17adfecbfe914bdf34cd717e
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-27 16:20:39 +0700

    UI

--- src/containers/pages/InboxPage/index.js
+++ src/containers/pages/InboxPage/index.js
@@ -1,19 +1,106 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+
+import { GoBanner } from '../../../components/molecules';
+import { HlpGfAts } from '../../../assets/icon';
 
 
 class InboxPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (
-            <View>
-                <Text>InboxPage</Text>
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+        <View style={{backgroundColor: 'white', flex: 1}}>
+            <View style={{borderBottomWidth: 1, borderBottomColor: '#f2f2f2', height: 65, justifyContent: 'center'}}>
+                <Text style={{color: 'black', fontSize: 20, fontWeight: 'bold', textAlign: 'center'}}>Inbox</Text>
             </View>
-        );
-    }
+            <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <View style={{marginHorizontal: 16, position: 'relative'}}>
+                        <Image source={HlpGfAts} style={{position: 'absolute'}} />
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 16, fontWeight: 'bold'}}>Burger untuk menemani nyemilmu!</Text>
+                        <Text style={{paddingLeft: 80, paddingRight: 10, textAlign: 'justify', fontSize: 15}}>Gampang! Pesan aja menu siap makan. Disini kamu akan mendapatkan harga menarik untuk cemilanmu.</Text>
+                    </View>
+                    <GoBanner />
+                </View>
+            </ScrollView>
+        </View>
+    );
+  }
 }
- 
+
 export default InboxPage;
