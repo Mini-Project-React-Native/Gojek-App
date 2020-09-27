daa49f429423355b4d0de73f255b4d9d82b8b6bd
fauzanmuhtadi <69455234+fauzanmuhtadi@users.noreply.github.com>
2020-09-27 16:31:54 +0700

    UI

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -24,5 +24,98 @@ import Promo from './promo.png';
 import Search from './search.png';
 import TopUp from './topup.png';
 import SmartphoneWhite from './smartphone-white.png';
+import ArrowRight from './arrow-right.png';
+import ChangeLanguage from './change-language.png';
+import EditProfile from './edit-profile.png';
+import EnterPromoCode from './enter-promo-code.png';
+import InviteAndEarn from './invite-and-earn.png';
+import MyOrders from './my-orders.png';
+import MyVouchers from './my-vouchers.png';
+import PrivacyPolicy from './privacy-policy.png';
+import ProfileAccount from './profile-512.png';
+import RateGojekApp from './rate-gojek-app.png';
+import TermsOfService from './terms-of-service.png';
+import HlpAam from './hlp-aam.png';
+import HlpAcf from './hlp-acf.png';
+import HlpAg from './hlp-ag.png';
+import HlpAi from './hlp-ai.png';
+import HlpApm from './hlp-apm.png';
+import HlpFat from './hlp-fat.png';
+import HlpGc from './hlp-gc.png';
+import HlpGfAts from './hlp-gf-ats.png';
+import HlpGfBwh from './hlp-gf-bwh.png';
+import HlpGp from './hlp-gp.png';
+import HlpGpls from './hlp-gpls.png';
+import HlpGr from './hlp-gr.png';
+import HlpGs from './hlp-gs.png';
+import HlpGsp from './hlp-gsp.png';
+import HlpGt from './hlp-gt.png';
+import HlpJaop from './hlp-jaop.png';
+import HlpOh from './hlp-oh.png';
+import HlpOs from './hlp-os.png';
+import HlpPl from './hlp-pl.png';
+import HlpSae from './hlp-sae.png';
+import ImgOrd from './img-ord.png';
+import LogoOrd from './logo-ord.png';
 
-export { AccountActive, Account, GoBluebird, GoCar, GoDeals, GoFood, GoMore, GoPulsa, GoRide, GoSend, Gopay, HelpActive, Help, HomeActive, Home, InboxActive, Inbox, More, Nearby, OrderActive, Order, Pay, Promo, Search, TopUp, SmartphoneWhite };
+export {
+    AccountActive,
+    Account,
+    GoBluebird,
+    GoCar, 
+    GoDeals,
+    GoFood,
+    GoMore,
+    GoPulsa,
+    GoRide,
+    GoSend,
+    Gopay,
+    HelpActive,
+    Help,
+    HomeActive,
+    Home,
+    InboxActive,
+    Inbox,
+    More,
+    Nearby,
+    OrderActive,
+    Order,
+    Pay,
+    Promo,
+    Search,
+    TopUp,
+    SmartphoneWhite,
+    ArrowRight,
+    ChangeLanguage,
+    EditProfile,
+    EnterPromoCode,
+    InviteAndEarn,
+    MyOrders,
+    MyVouchers,
+    PrivacyPolicy,
+    ProfileAccount,
+    RateGojekApp,
+    TermsOfService,
+    HlpAam,
+    HlpAcf,
+    HlpAg,
+    HlpAi,
+    HlpApm,
+    HlpFat,
+    HlpGc,
+    HlpGfAts,
+    HlpGfBwh,
+    HlpGp,
+    HlpGpls,
+    HlpGr,
+    HlpGs,
+    HlpGsp,
+    HlpGt,
+    HlpJaop,
+    HlpOh,
+    HlpOs,
+    HlpPl,
+    HlpSae,
+    ImgOrd,
+    LogoOrd
+};
